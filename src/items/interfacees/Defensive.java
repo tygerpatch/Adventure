e4581007faf86f57127f606cf88a2b912721b0c1
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-01 12:49:16 -0400

    Added method useOn(BadGuy) for handling the logic involved with defending against a BadGuy.

--- src/items/interfacees/Defensive.java
+++ src/items/interfacees/Defensive.java
@@ -1,4 +1,9 @@
 package items.interfacees;
 
+import characters.BadGuy;
+
 public interface Defensive {
+  // You only need to defend against bad guys. (ie. Hag, Troll, Vampire, Werewolf)
+  // Return true if Item kills BadGuy, false otherwise.
+  public boolean useOn(BadGuy badGuy);
 }
