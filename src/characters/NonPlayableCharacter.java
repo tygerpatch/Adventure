896ed5ccd79142b0dd9f561e3c1ec99fe18305a2
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-01 12:37:37 -0400

    Simplified NonPlayableCharacter class.
    Moved code for defending against NonPlayableCharacter into Defensive objects.
    For example, the Defensive object Club handles the code for clubbing BadGuys.

--- src/characters/NonPlayableCharacter.java
+++ src/characters/NonPlayableCharacter.java
@@ -1,23 +1,8 @@
 package characters;
 
-import java.util.Scanner;
-
 import stuff.Player;
 
 public abstract class NonPlayableCharacter {
-  protected boolean enchanted = false;
-
   public abstract void interactWith(Player player);
-  public abstract void clubbed();
-  public abstract void shot();
-  public abstract void staked();
-  public abstract void enchanted();
-
-  public void unenchant() {
-    enchanted = false;
-  }
-
-  public abstract boolean isAlive();
-  public abstract String toString();
-  public abstract void wakeUp();
+  public abstract String getName();
 }
