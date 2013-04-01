025efa16a8e8698998c18f5e0d3bf8903dc0931b
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-01 13:55:29 -0400

    ItemParser should return new instances of Items, instead of cached references.

--- src/util/ItemParser.java
+++ src/util/ItemParser.java
@@ -16,45 +16,69 @@ import items.classes.Tome;
 import items.classes.WoodenStake;
 import items.interfacees.Item;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class ItemParser {
-  private static final List<Item> items = new LinkedList<Item>();
-
-  static {
-    // Defensive Items
-    items.add(new Club());
-    items.add(new SilverBullet());
-    items.add(new Spell());
-    items.add(new WoodenStake());
-
-    // Drinkable Health Items
-    items.add(new Elixir());
-
-    // Eatable Defensive Items
-    items.add(new Garlic());
-
-    // Eatable Health Item
-    items.add(new Bread());
-
-    // Moveable Items
-    items.add(new Crown());
-    items.add(new Goblet());
-    items.add(new Jewel());
-    items.add(new Tome());
-
-    // Unmoveable Items
-    items.add(new Candle());
-    items.add(new Table());
-    items.add(new Nothing());
-  }
 
   public static Item parseString(String strItem) {
-    for(Item item : items) {
-      if(item.getName().equalsIgnoreCase(strItem)) {
-        return item;
-      }
+    // *** Defensive Items
+    if("Club".equalsIgnoreCase(strItem)) {
+      return new Club();
+    }
+
+    if("Silver-Bullet".equalsIgnoreCase(strItem)) {
+      return new SilverBullet();
+    }
+
+    if("Spell".equalsIgnoreCase(strItem)) {
+      return new Spell();
+    }
+
+    if("Wooden-Stake".equalsIgnoreCase(strItem)) {
+      return new WoodenStake();
+    }
+
+    // *** Drinkable Health Items
+    if("Elixir".equalsIgnoreCase(strItem)) {
+      return new Elixir();
+    }
+
+    // *** Eatable Defensive Items
+    if("Garlic".equalsIgnoreCase(strItem)) {
+      return new Garlic();
+    }
+
+    // *** Eatable Health Item
+    if("Bread".equalsIgnoreCase(strItem)) {
+      return new Bread();
+    }
+
+    // *** Unmoveable Items
+    if("Candle".equalsIgnoreCase(strItem)) {
+      return new Candle();
+    }
+
+    if("Table".equalsIgnoreCase(strItem)) {
+      return new Table();
+    }
+
+    if("Nothing".equalsIgnoreCase(strItem)) {
+      return new Nothing();
+    }
+
+    // *** Items
+    if("Crown".equalsIgnoreCase(strItem)) {
+      return new Crown();
+    }
+
+    if("Goblet".equalsIgnoreCase(strItem)) {
+      return new Goblet();
+    }
+
+    if("Jewel".equalsIgnoreCase(strItem)) {
+      return new Jewel();
+    }
+
+    if("Tome".equalsIgnoreCase(strItem)) {
+      return new Tome();
     }
 
     return null;
