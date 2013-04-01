eef568f90449caa75d7f46567a179d1795d775cc
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-01 12:28:59 -0400

    Added methods to indicate whether or not a BadGuy is blocking the doors in a Room.

--- src/characters/BadGuy.java
+++ src/characters/BadGuy.java
@@ -2,7 +2,10 @@ package characters;
 
 import stuff.Player;
 
-// There are good and bad guys. Bad guys damage player
+// There are good and bad guys. Bad guys damage player.
 public interface BadGuy {
   public void damage(Player player);
+  public void setBlockingDoor(boolean isBlockingDoor);
+  public boolean isBlockingDoor();
+  public String getName();
 }
