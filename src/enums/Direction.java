c020230333de694cb3df9faf8170d0fcf8180e31
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-02 19:03:23 -0400

    Deleted Direction's fromInteger(int) and nextClockwise() methods

--- src/enums/Direction.java
+++ src/enums/Direction.java
@@ -3,25 +3,6 @@ package enums;
 public enum Direction {
   North, East, South, West;
 
-  // ex. Direction.North.nextClockwise() -> Direction.East
-  public Direction nextClockwise() {
-    if(Direction.North == this) {
-      return Direction.East;
-    }
-
-    if(Direction.East == this) {
-      return Direction.South;
-    }
-
-    if(Direction.South == this) {
-      return Direction.West;
-    }
-
-    // if(Direction.West == this)...
-    return Direction.North;
-  }
-
-  // ex. Direction.valueOf('N') -> Direction.North
   public static Direction fromChar(char ch) {
     if((ch == 'N') || (ch == 'n')) {
       return North;
@@ -37,26 +18,6 @@ public enum Direction {
 
     if((ch == 'W') || (ch == 'w')) {
       return West;
-    }
-
-    return null;
-  }
-
-  public static Direction fromInteger(int i) {
-    if(Direction.North.ordinal() == i) {
-      return Direction.North;
-    }
-
-    if(Direction.East.ordinal() == i) {
-      return Direction.East;
-    }
-
-    if(Direction.South.ordinal() == i) {
-      return Direction.South;
-    }
-
-    if(Direction.West.ordinal() == i) {
-      return Direction.West;
     }
 
     return null;
