fdc8c631e952217900f6a6858b61f05face1f850
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-02 18:48:54 -0400

    * DebugCommand no longer extends from AbstractCommand * Added main method to DebugCommand to assist in testing * Refactored DebugCommand's execute method

--- src/commands/DebugCommand.java
+++ src/commands/DebugCommand.java
@@ -1,38 +1,46 @@
 package commands;
 
-import stuff.Adventure;
+import java.io.FileNotFoundException;
+
+import stuff.Castle;
+import stuff.Door;
 import stuff.Room;
 import enums.Direction;
 
-public class DebugCommand extends AbstractCommand {
-
-  public DebugCommand(Adventure adventure) {
-    super(adventure);
-  }
+public class DebugCommand {
 
   // Debug – this is so that I can verify you built your graph correctly.
   // Output should be a list of rooms and connections, one per line.
   // example: kitchen north to living room
-  @Override
-  public void execute() {
-    Room[] castle = adventure.getCastle();
-    Room destination;
-    Direction direction = Direction.North;
-
-    // for each room in the castle
-    for (Room origin : castle) {
-      if (null != origin) {
-        // for each door in the room
-        for (int door : origin.getDoors()) {
-          // if the door leads to another room
-          if (null != (destination = castle[door])) {
-            // display debugging information (ex. kitchen north to living room)
-            System.out.println(origin + " " + direction + " to " + destination);
-          }
-          // ex. North -> East, East -> South
-          direction = direction.nextClockwise();
-        }
-      }
+  public void execute(Castle castle) {
+    for(Room room : castle) {
+      debug(castle, room, Direction.North);
+      debug(castle, room, Direction.East);
+      debug(castle, room, Direction.South);
+      debug(castle, room, Direction.West);
     }
+  }
+
+  private void debug(Castle castle, Room fromRoom, Direction direction) {
+    Door door = fromRoom.getDoor(direction);
+
+    if(door == null) {
+      return;
+    }
+
+    // get the room the door opens into
+    Room toRoom = castle.getRoom(door.open());
+
+    // ex. Kitchen North to Living Room.
+    System.out.print(fromRoom.getName());
+    System.out.print(" ");
+    System.out.print(direction.getName());
+    System.out.print(" to ");
+    System.out.print(toRoom.getName());
+    System.out.println(".");
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    new DebugCommand().execute(Castle.loadFrom("./dat-files/castle.dat"));
   }
 }
