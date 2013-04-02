c10264a5a36e40af53470ad945a94771f74b7b69
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-02 18:24:09 -0400

    * Added main method to Room class to assist in testing * Merged FleeCommand's inner Node class into Room class * Room now stores an array of Doors, instead of an array of ints * Room no longer returns all its Doors via getDoors.  Clients must now use getDoor(Direction) * Room's items are no longer public.  Clients can use either addItem or removeItem. * Renamed method setOccupant to addOccupant * Added method to test for and retrieve the BadGuy in the Room

--- src/stuff/Room.java
+++ src/stuff/Room.java
@@ -1,11 +1,14 @@
 package stuff;
 
-import items.interfacees.Item;
+import items.classes.Garlic;
+import items.classes.Item;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import characters.BadGuy;
 import characters.NonPlayableCharacter;
+import characters.Wizard;
 import enums.Direction;
 
 // Class to represent a room in the Adventure program
@@ -14,42 +17,13 @@ import enums.Direction;
 public class Room {
 
   private String name;
-  private int[] doors = {0, 0, 0, 0};
 
   public void setName(String name) {
     this.name = name;
   }
 
-  public void addDoor(Direction direction, int destination) {
-    doors[direction.ordinal()] = destination;
-  }
-
-  public int[] getDoors() {
-    return doors;
-  }
-
-  public String toString() {
+  public String getName() {
     return name;
-  }
-
-  public List<Item> items =  new LinkedList<Item>();
-
-  private NonPlayableCharacter occupant;
-
-  public NonPlayableCharacter getOccupant() {
-    return occupant;
-  }
-
-  public void setOccupant(NonPlayableCharacter occupant) {
-    this.occupant = occupant;
-  }
-
-  public boolean hasDoor(Direction direction) {
-    return (0 != doors[direction.ordinal()]);
-  }
-
-  public boolean hasOccupant() {
-    return (occupant != null);
   }
 
   private int number = 0;
@@ -62,7 +36,105 @@ public class Room {
     this.number = num;
   }
 
-  public String getName() {
-    return name;
+  private Door[] doors = new Door[4];
+
+  public void addDoor(Direction direction, int destination) {
+    Door door = new Door();
+    door.setDestination(destination);
+
+    doors[direction.ordinal()] = door;
+  }
+
+  // Will return null if there is no door in the given direction
+  public Door getDoor(Direction direction) {
+    return doors[direction.ordinal()];
+  }
+
+  private List<Item> items =  new LinkedList<Item>();
+
+  public void addItem(Item item) {
+    items.add(item);
+  }
+
+  public void removeItem(Item item) {
+    items.remove(item);
+  }
+
+  public boolean hasItems() {
+    return (items.size() > 0);
+  }
+
+  public List<Item> getItems() {
+    return items;
+  }
+
+  private NonPlayableCharacter occupant;
+
+  public NonPlayableCharacter getOccupant() {
+    return occupant;
+  }
+
+  public void addOccupant(NonPlayableCharacter occupant) {
+    this.occupant = occupant;
+  }
+
+  public boolean hasOccupant() {
+    return (occupant != null);
+  }
+
+  public boolean hasBadGuy() {
+    return (occupant instanceof BadGuy);
+  }
+
+  public BadGuy getBadGuy() {
+    return (BadGuy) occupant;
+  }
+
+  // ** methods for Dijkstra's Algorithm
+  private int distance;
+
+  public void setDistance(int distance) {
+    this.distance = distance;
+  }
+
+  public int getDistance() {
+    return distance;
+  }
+
+  private boolean visited;
+
+  public void setVisited(boolean visited) {
+    this.visited = visited;
+  }
+
+  public boolean wasVisited() {
+    return visited;
+  }
+
+  private Path path;
+
+  // Set the path I took to get to this Room.
+  public void setPath(Path path) {
+    this.path = path;
+  }
+
+  // Get the path I took to get to this Room.
+  public Path getPath() {
+    return path;
+  }
+
+  public static void main(String[] args) {
+    Room room = new Room();
+    System.out.println("-- Test methods on a Room that DOES NOT have an occupant or any Items --");
+    System.out.println("Does the room have an occupant? " + (room.hasOccupant() ? "Yes" : "No"));
+    System.out.println("Does the room have any items? " + (room.hasItems() ? "Yes" : "No"));
+
+    System.out.println("-- Test that a Room DOES have an occupant and some items --");
+    NonPlayableCharacter occupant = new Wizard();
+    room.addOccupant(occupant);
+    System.out.println("Does the room have an occupant? " + (room.hasOccupant() ? "Yes" : "No"));
+
+    room.addItem(new Garlic());
+    System.out.println("Does the room have any items? " + (room.hasItems() ? "Yes" : "No"));
   }
 }
