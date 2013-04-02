374cb8d8bf66b552c4547b2551d2c00ac13e08c9
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-02 18:55:10 -0400

    Refactored Adventure class so that it just contains one static void main method.

--- src/stuff/Adventure.java
+++ src/stuff/Adventure.java
@@ -1,25 +1,13 @@
 package stuff;
 
 import items.classes.Crown;
-import items.interfacees.Item;
+import items.classes.Item;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import util.ItemParser;
-import util.NonPlayableCharacterParser;
-import characters.NonPlayableCharacter;
-
-import commands.AbstractCommand;
-import commands.ContentsCommand;
 import commands.DebugCommand;
 import commands.FleeCommand;
-import commands.QuitCommand;
 import commands.RemoveCommand;
 import commands.TakeCommand;
 import commands.UseCommand;
@@ -27,9 +15,6 @@ import commands.moving.EastMovementCommand;
 import commands.moving.NorthMovementCommand;
 import commands.moving.SouthMovementCommand;
 import commands.moving.WestMovementCommand;
-
-import enums.Direction;
-// import static enums.Direction.*;
 
 // Adventure is a text based action/adventure game.
 // Player navigates a complex castle, picking up tools and treasures, while warding off evil beings.
@@ -56,132 +41,89 @@ public class Adventure {
     MENU.append("D, d\tDebug");
   }
 
-  public static void main(String[] args) throws IOException {
-    Adventure game = new Adventure();
+  public static void main(String[] args) throws FileNotFoundException {
 
-    game.loadCastle("./dat-files/castle.dat");
-    game.play();
-  }
-
-  // Rooms will be numbered consecutively, starting at 1, with a maximum of 98 rooms.
-  private Room[] castle;
-
-  // 99 indicating no more rooms
-  public static final int NO_MORE_ROOMS = 99;
-
-  // 99 indicating no more door information
-  public static final int NO_MORE_PASSAGES = 99;
-
-  private Room currentRoom, entranceHall;
-
-  public void loadCastle(String filePath) throws IOException {
-    Scanner scanner = new Scanner(new File(filePath));
-
-    // Step 1: Read in all the rooms that are in the castle.
-    // node and description: an integer followed by a space and up to 20 characters for the room name
-    int node;
-    String description;
-    Room room;
-    List<Room> rooms = new LinkedList<Room>();
-
-    while (NO_MORE_ROOMS != (node = scanner.nextInt())) {
-      // create a new room with the given name and number
-      room = new Room();
-      description = scanner.nextLine().trim();
-
-      room.setName(description);
-      room.setNumber(node);
-
-      rooms.add(room);
-
-      // The user starts in the Entrance Hallway.
-      if ("Entrance Hall".equalsIgnoreCase(description)) {
-        currentRoom = entranceHall = room;
-      }
-    }
-
-    castle = (Room[]) rooms.toArray();
-
-    // Step 2: Read in all the passages in the castle
-    // Indicated by triplets (room#, room#, direction): integer, integer, 1 character
-    int origin, destination;
-    char ch;
-
-    while (NO_MORE_PASSAGES != (origin = scanner.nextInt())) {
-      destination = scanner.nextInt();
-
-      ch = scanner.nextLine().trim().charAt(0);
-      castle[origin].addDoor(Direction.parseChar(ch), destination);
-    }
-
-    // Step 3: Read in the details about each room.
-    // Indicated by by room #, number of items in room, list of items in room
-    int numItems;
-    String strItem;
-    Item item;
-    NonPlayableCharacter npc;
-
-    // keep reading until end of stream is reached
-    while (scanner.hasNextInt()) {
-      room = castle[scanner.nextInt()];
-      numItems = scanner.nextInt();
-
-      for(int i = 0; i < numItems; i++) {
-        strItem = scanner.next();
-
-        item = ItemParser.parseString(strItem);
-
-        if(null != item) {
-          room.items.add(item);
-        }
-        else {
-          npc = NonPlayableCharacterParser.parseString(strItem, this.scanner);
-          room.setOccupant(npc);
-        }
-      }
-    }
-
-    scanner.close();
-  }
-
-  private Player player = new Player();
-
-  private boolean stillPlaying;
-  private Scanner scanner;
-
-  public void play() {
+    Castle castle = Castle.loadFrom("./dat-files/castle.dat");
     System.out.println("King: Find me my crown!");
-
-    // Tell the user what room he/she is in
-    System.out.println("You are now in the " + currentRoom);
-
-    // Tell the user what and/or who is in the room with you.
-    System.out.println("Items in the room: " + currentRoom.items);
-
-    if(currentRoom.hasOccupant()) {
-      System.out.println("The room also has an occupant: " + currentRoom.getOccupant().getName());
-    }
-
-    //Then use a loop that allows the user to interactively explore the castle
-    // by entering one-letter directions (ex. E, e, W, w, N, n, S, s, F, f, D, d, Q, q).
+    Player player = new Player();
+    player.setRoom(castle.findRoom("Entrance Hall"));
     char ch;
-    stillPlaying = true;
-    AbstractCommand command;
+
+    UseCommand useCommand = new UseCommand();
+    TakeCommand takeCommand = new TakeCommand();
+    RemoveCommand removeCommand = new RemoveCommand();
+    DebugCommand debugCommand = new DebugCommand();
+    FleeCommand fleeCommand = new FleeCommand();
+
+    NorthMovementCommand northCommand = new NorthMovementCommand();
+    EastMovementCommand eastCommand = new EastMovementCommand();
+    SouthMovementCommand southCommand = new SouthMovementCommand();
+    WestMovementCommand westCommand = new WestMovementCommand();
 
     System.out.println(MENU.toString());
     System.out.println();
 
-    while (stillPlaying) {
+    Scanner scanner = new Scanner(System.in);
+    boolean done = false;
+
+    while (!done && player.isAlive()) {
       System.out.print("Enter command: ");
 
       ch = scanner.nextLine().trim().toUpperCase().charAt(0);
-      command = mapCommands.get(Character.valueOf(ch));
 
-      if(null == command) {
-        System.out.println("I'm sorry. I don't understand that command.");
+      // Use – use an item from the knapsack
+      if('U' == ch) {
+        useCommand.execute(player, scanner, player.getRoom());
+      }
+      // Take – pick up the item (same as Use)
+      else if('T' == ch) {
+        takeCommand.execute(player.getRoom(), player.knapsack, scanner);
+      }
+      // Remove – remove an item from the knapsack (same as Use) but leave in room
+      else if('R' == ch) {
+        removeCommand.execute(player.knapsack, scanner, player.getRoom());
+      }
+      // Debug – this is so that I can verify you built your graph correctly.
+      // Output should be a list of rooms and connections, one per line.
+      // example: kitchen north to living room
+      else if('D' == ch) {
+        debugCommand.execute(castle);
+      }
+      // Flee – take the shortest path to the entry hallway to exit the castle
+      else if('F' == ch) {
+        fleeCommand.execute(castle, player.getRoom());
+        done = true;
+      }
+      // Quit the game
+      else if('Q' == ch) {
+        done = true;
+      }
+      // Contents – list the contents of the knapsack
+      else if('C' == ch) {
+        if(player.knapsack.isEmpty()) {
+          System.out.println("You do not have anything in your knapsack.");
+        }
+        else {
+          System.out.println("In your knapsack you have: " + player.knapsack);
+        }
+      }
+      // North – move through the north door if there is one
+      else if('N' == ch) {
+        northCommand.execute(player, castle);
+      }
+      // East – move through the east door if there is one
+      else if('E' == ch) {
+        eastCommand.execute(player, castle);
+      }
+      // South – move through the south door if there is one
+      else if('S' == ch) {
+        southCommand.execute(player, castle);
+      }
+      else if('W' == ch) {
+        westCommand.execute(player, castle);
       }
       else {
-        command.execute();
+        System.out.println("That is not a valid command.");
       }
     }
 
@@ -199,74 +141,5 @@ public class Adventure {
       System.out.println("Your head is chopped off by the King's executioner.");
       System.out.println("There's a lot of blood.");
     }
-  }
-
-  public Room[] getCastle() {
-    return castle;
-  }
-
-  public Room getCurrentRoom() {
-    return currentRoom;
-  }
-
-  public Room getEntranceHall() {
-    return entranceHall;
-  }
-
-  public void setStillPlaying(boolean flag) {
-    stillPlaying = flag;
-  }
-
-  public Player getPlayer() {
-    return player;
-  }
-
-  private Map<Character, AbstractCommand> mapCommands = new HashMap<Character, AbstractCommand>();
-
-  public Adventure() {
-    scanner = new Scanner(System.in);
-
-    // Use – use an item from the knapsack
-    mapCommands.put(Character.valueOf('U'), new UseCommand(this));
-
-    // Take – pick up the item (same as Use)
-    mapCommands.put(Character.valueOf('T'), new TakeCommand(this));
-
-    // Remove – remove an item from the knapsack (same as Use) but leave in room
-    mapCommands.put(Character.valueOf('R'), new RemoveCommand(this));
-
-    // Debug – this is so that I can verify you built your graph correctly.
-    // Output should be a list of rooms and connections, one per line.
-    // example: kitchen north to living room
-    mapCommands.put(Character.valueOf('D'), new DebugCommand(this));
-
-    // Flee – take the shortest path to the entry hallway to exit the castle
-    mapCommands.put(Character.valueOf('F'), new FleeCommand(this));
-
-    // Quit the game
-    mapCommands.put(Character.valueOf('Q'), new QuitCommand(this));
-
-    // Contents – list the contents of the knapsack
-    mapCommands.put(Character.valueOf('C'), new ContentsCommand(this));
-
-    // North – move through the north door if there is one
-    mapCommands.put(Character.valueOf('N'), new NorthMovementCommand(this));
-
-    // South – move through the south door if there is one
-    mapCommands.put(Character.valueOf('S'), new SouthMovementCommand(this));
-
-    // East – move through the east door if there is one
-    mapCommands.put(Character.valueOf('E'), new EastMovementCommand(this));
-
-    // West – move through the west door if there is one
-    mapCommands.put(Character.valueOf('W'), new WestMovementCommand(this));
-  }
-
-  public void setCurrentRoom(Room room) {
-    this.currentRoom = room;
-  }
-
-  public Scanner getScanner() {
-    return this.scanner;
   }
 }
