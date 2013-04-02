3b0a9bc8cef0b01e4f5359ffdf22971deb60dbc4
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-04-02 08:08:18 -0400

    * Renamed NonPlayableCharacterParser's parseString method to fromString * Moved NonPlayableCharacterParser's fromString method into NonPlayableCharacter interface * NonPlayableCharacter is no longer an interface.  It's now an abstract class. * Deleted NonPlayableCharacterParser

--- src/characters/NonPlayableCharacter.java
+++ src/characters/NonPlayableCharacter.java
@@ -1,8 +1,38 @@
 package characters;
 
+import java.util.Scanner;
+
 import stuff.Player;
 
-public interface NonPlayableCharacter {
-  public void interactWith(Player player);
-  public String getName();
+public abstract class NonPlayableCharacter {
+  public abstract void interactWith(Player player);
+  public abstract String getName();
+
+  public static final NonPlayableCharacter fromString(String str, Scanner scanner) {
+    if("Hag".equalsIgnoreCase(str)) {
+      return new Hag(scanner);
+    }
+
+    if("Hunchback".equalsIgnoreCase(str)) {
+      return new Hunchback();
+    }
+
+    if("Troll".equalsIgnoreCase(str)) {
+      return new Troll();
+    }
+
+    if("Vampire".equalsIgnoreCase(str)) {
+      return new Vampire();
+    }
+
+    if("Werewolf".equalsIgnoreCase(str)) {
+      return new Werewolf();
+    }
+
+    if("Wizard".equalsIgnoreCase(str)) {
+      return new Wizard();
+    }
+
+    return null;
+  }
 }
